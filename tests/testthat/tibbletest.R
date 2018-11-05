ae3f1906cfa540cfc1aab3ab4690ef4149b4f2ce
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2018-11-05 17:29:32 -0500

    Working tibbletest.R

--- tests/testthat/tibbletest.R
+++ tests/testthat/tibbletest.R
@@ -2,8 +2,7 @@ context("Data format")
 library(farsr)
 
 test_that("imported csv is correctly converted to tbl_df format", {
-  source(file.path("R", "fars_functions.R"))
-  acc_file <- file.path("inst", "extdata", "accident_2013.csv")
+  acc_file <- system.file("extdata", "accident_2013.csv", package = "farsr")
   acc13 <- fars_read(acc_file)
   expect_that(acc13, is_a("tbl_df"))
 })
