b02b0c7d38df35cd56fede5bd2d0ad1d360527c8
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2018-11-05 16:56:37 -0500

    Added test_that function to tibbletest.R

--- inst/tests/tibbletest.R
+++ inst/tests/tibbletest.R
@@ -1,5 +1,6 @@
-source("R/fars_functions.R")
-acc_file <- file.path("inst", "extdata", "accident_2013.csv")
-acc13 <- fars_read(acc_file)
-
-expect_that(acc13, is_a("tbl_df"))
+test_that("imported csv is correctly converted to tbl_df format", {
+  source("R/fars_functions.R")
+  acc_file <- file.path("inst", "extdata", "accident_2013.csv")
+  acc13 <- fars_read(acc_file)
+  expect_that(acc13, is_a("tbl_df"))
+})
